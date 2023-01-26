3f2931ec3d2d7b1c78464eb43b8c38265fb3b3ea
Yasmin <ysnsaless@gmail.com>
2023-01-25 21:21:52 -0300

    navegação pags 3.1 e 3.2

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,24 @@ function criarPerguntas() {
     let niveis = document.querySelector('.niveis').value;
 
     if ((titulo.length>19 && titulo.length<66) && (imagem.startsWith("http://") || imagem.startsWith("https://")) && Number(qtdPerguntas)>2 && niveis>1){
-        // document.querySelector('.InfoBasica').classList.add('escondido');
-       // document.querySelector('.Perguntas').classList.remove('escondido');
+        document.querySelector('.InfoBasica').classList.add('escondido');
+        document.querySelector('.Perguntas').classList.remove('escondido');
     }
     else {
         alert('n pode')
    }
+}
+
+function criarNiveis(){
+    let pergunta = document.querySelector('.pergunta').value;
+    let fundo = document.querySelector('.fundo').value;
+    let resposta = document.querySelector('.resposta').value;
+    let urlimagem = document.querySelector('.urlimagem').value;
+    //let Incorreta = document.querySelector('.resposta .Incorreta').value
+    //console.log(Incorreta);
+    if (pergunta.length>19 && (fundo.startsWith("#") && fundo.length===7) && resposta !=="" && (urlimagem.startsWith("http://") || urlimagem.startsWith("https://"))){
+        document.querySelector('.Perguntas').classList.add('escondido');
+    }else {
+        alert('n pode');
+    }
 }
